503715a81c9c040587029eddcc2cda1aa6852d36
OjaF <rauzatuljf2002@gmail.com>
2024-03-22 21:14:24 +0700

    feat: menambahkan prettier tailwind css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>SPK - Tanah</title>  
+        <title>SPK - Tanah</title>
 
         {{-- Tailwind --}}
         @vite('resources/css/app.css')
     </head>
     <body>
-        <div class="min-h-screen flex flex-col items-center justify-center bg-primary">
-            <div class="p-3 bg-white rounded-md">
+        <div class="flex min-h-screen flex-col items-center justify-center bg-primary">
+            <div class="rounded-md bg-white p-3">
                 {{-- Header --}}
-                <div>
-                    <div class="flex gap-2">
-                        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="mx-auto w-20 h-20">
-                        <h1 class="text-3xl font-bold text-center">SPK - Tanah</h1>
+                <div class="w-full">
+                    <div class="flex w-full justify-center gap-2">
+                        <img src="{{ asset('images/database.svg') }}" alt="Logo" class="h-8 w-8" />
+                        <h1 class="text-center text-2xl font-bold">SPK - Tanah</h1>
                     </div>
                     <p class="text-center">Sistem Pendukung Keputusan Penentuan Harga Tanah</p>
                 </div>
